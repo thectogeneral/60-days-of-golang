6ea9bbdcca71ae94ee108c44c7ba4d31b4f56512
thectogeneral <salemolorundare@gmail.com>
2024-10-22 21:39:15 +0100

    Interface in Golang

---
+++ Day 12 - Introduction to Interfaces/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Rectangle struct {
+	Width, Height float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func main() {
+	c := Circle{Radius: 5}
+	r := Rectangle{Width: 4, Height: 6}
+
+	fmt.Println("Circle Area:", c.Area())
+	fmt.Println("Rectangle Area:", r.Area())
+}
